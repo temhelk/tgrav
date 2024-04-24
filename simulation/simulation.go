c945aac48ea3abb8786066aacf9fc622922ff597
temhelk <temhelk@gmail.com>
2024-04-24 15:18:31 +0300

    Better acceleration calculations

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -8,10 +8,10 @@ const G float64 = 6.674e-11
 
 type Body struct {
 	Mass     float64
+
+	Acceleration r2.Vec
 	Position r2.Vec
 	Velocity r2.Vec
-
-	appliedForce r2.Vec
 }
 
 type Simulation struct {
@@ -38,7 +38,7 @@ func (sim *Simulation) Step() {
 	for bodyIndex := range sim.Bodies {
 		body := &sim.Bodies[bodyIndex]
 
-		body.appliedForce = r2.Vec{X: 0, Y: 0}
+		body.Acceleration = r2.Vec{X: 0, Y: 0}
 
 		for otherBodyIndex, otherBody := range sim.Bodies {
 			if otherBodyIndex == bodyIndex {
@@ -46,20 +46,18 @@ func (sim *Simulation) Step() {
 			}
 
 			bodyToOtherBody := r2.Sub(otherBody.Position, body.Position)
-			forceAmplitude := G * (body.Mass * otherBody.Mass) / r2.Norm2(bodyToOtherBody)
-			force := r2.Scale(forceAmplitude, r2.Unit(bodyToOtherBody))
+			accelerationAmplitude := G * otherBody.Mass / r2.Norm2(bodyToOtherBody)
+			acceleration := r2.Scale(accelerationAmplitude, r2.Unit(bodyToOtherBody))
 
-			body.appliedForce = r2.Add(body.appliedForce, force)
+			body.Acceleration = r2.Add(body.Acceleration, acceleration)
 		}
 	}
 
-	// Apply forces to all bodies
+	// Apply acceleration to all bodies
 	for bodyIndex := range sim.Bodies {
 		body := &sim.Bodies[bodyIndex]
 
-		acceleration := r2.Scale(1/body.Mass, body.appliedForce)
-
-		body.Velocity = r2.Add(body.Velocity, r2.Scale(sim.TimeStep, acceleration))
+		body.Velocity = r2.Add(body.Velocity, r2.Scale(sim.TimeStep, body.Acceleration))
 		body.Position = r2.Add(body.Position, r2.Scale(sim.TimeStep, body.Velocity))
 	}
 }
